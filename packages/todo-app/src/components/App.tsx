d977d906dbf3c2184068498c4da9ccf06354bbb9
Chinatsu Kato <cnt.tkhs@gmail.com>
2021-01-14 09:10:57 +0900

    Create todo app

---
+++ packages/todo-app/src/components/App.tsx
@@ -0,0 +1,8 @@
+import { RecoilRoot } from 'recoil'
+import { CharacterCounter } from './CharacterCounter'
+
+export const App = () => (
+  <RecoilRoot>
+    <CharacterCounter />
+  </RecoilRoot>
+)
