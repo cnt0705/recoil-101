442ae92b46705af1fda7db78d1f5dba1d5bc0727
Chinatsu Kato <cnt.tkhs@gmail.com>
2021-01-16 15:07:30 +0900

    Add todo to Recoil state

--- packages/todo-app/src/components/TodoItemCreator.tsx
+++ packages/todo-app/src/components/TodoItemCreator.tsx
@@ -7,5 +7,22 @@ export const TodoItemCreator = () => {
   const [inputValue, setInputValue] = useState('')
   const setTodoList = useSetRecoilState(todoListState)
 
-  return <div>TodoItemCreator</div>
+  const addItem = () => {
+    setTodoList(oldTodoList => [
+      ...oldTodoList,
+      { id: Math.random().toString(), text: inputValue },
+    ])
+    setInputValue('')
+  }
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value)
+  }
+
+  return (
+    <div>
+      <input type="text" value={inputValue} onChange={onChange} />
+      <button onClick={addItem}>Add</button>
+    </div>
+  )
 }
