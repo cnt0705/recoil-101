3e8f6a1fb27461ffc04ae3279591d0a109c2709e
Chinatsu Kato <cnt.tkhs@gmail.com>
2021-01-16 15:42:55 +0900

    Add filteredTodoListState

--- packages/todo-app/src/state/index.ts
+++ packages/todo-app/src/state/index.ts
@@ -1,7 +1,29 @@
-import { atom } from 'recoil'
+import { atom, selector } from 'recoil'
 import { TodoItemType } from '../types'
 
 export const todoListState = atom<TodoItemType[]>({
   key: 'todoListState',
   default: [],
+})
+
+export const todoListFilterState = atom({
+  key: 'todoListFilterState',
+  default: 'Show All',
+})
+
+export const filteredTodoListState = selector({
+  key: 'filteredTodoListState',
+  get: ({ get }) => {
+    const filter = get(todoListFilterState)
+    const list = get(todoListState)
+
+    switch (filter) {
+      case 'Show Completed':
+        return list.filter(item => item.isComplete)
+      case 'Show Uncompleted':
+        return list.filter(item => !item.isComplete)
+      default:
+        return list
+    }
+  },
 })
